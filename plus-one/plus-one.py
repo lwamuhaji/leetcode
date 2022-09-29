5ed68005f05a9bc35e95925424110afee4124d9b
lwamuhaji <61176508+lwamuhaji@users.noreply.github.com>
2022-09-29 16:12:47 +0900

    Time: 53 ms, Memory: 13.9 MB - LeetHub

--- plus-one/plus-one.py
+++ plus-one/plus-one.py
@@ -1,4 +1,4 @@
-class Solution2:
+class Solution:
     def plusOne(self, digits: List[int]) -> List[int]:
         for i, n in enumerate(digits[::-1]):
             n += 1
@@ -10,17 +10,3 @@ class Solution2:
                 digits[len(digits)-i-1] = n
                 break
         return digits
-    
-class Solution:
-    def plusOne(self, digits: List[int]) -> List[int]:
-        num = ""
-        for i in digits:
-            num = num + str(i)
-        
-        num = int(num) + 1
-        num = str(num)
-        
-        list = []
-        for i in num:
-            list.append(int(i))
-        return list
