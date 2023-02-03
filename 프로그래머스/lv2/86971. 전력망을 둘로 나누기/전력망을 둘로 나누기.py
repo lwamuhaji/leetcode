8943d0a86d8477fe3c28ec4184f6e5dea258778f
lwamuhaji <61176508+lwamuhaji@users.noreply.github.com>
2023-02-03 17:21:08 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 10.32 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.py
+++ 프로그래머스/lv2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.py
@@ -1,22 +1,28 @@
+uf = []
 
+def find(a):
+    global uf
+    if uf[a] < 0: return a
+    uf[a] = find(uf[a])
+    return uf[a]
 
-def count_connected(node, wires):
-    pool = [node]
-    i = 0
-    while i < len(pool):
-        for w in wires:
-            if w[0] == pool[i] or w[1] == pool[i]:
-                target = w[0] + w[1] - pool[i]
-                if target not in pool:
-                    pool.append(target)
-        i += 1
-    return len(pool)
+def merge(a, b):
+    global uf
+    pa = find(a)
+    pb = find(b)
+    if pa == pb: return
+    uf[pa] += uf[pb]
+    uf[pb] = pa
 
 def solution(n, wires):
-    min_ = n
-    for i in range(len(wires)):
-        a = count_connected(wires[i][0], wires[:i] + wires[i+1:])
-        b = count_connected(wires[i][1], wires[:i] + wires[i+1:])
-        if abs(a - b) < min_:
-            min_ = abs(a - b)
-    return min_
+    global uf
+    answer = int(1e9)
+    k = len(wires)
+    for i in range(k):
+        uf = [-1 for _ in range(n+1)]
+        tmp = [wires[x] for x in range(k) if x != i]
+        for a, b in tmp: merge(a, b)
+        v = [x for x in uf[1:] if x < 0]
+        answer = min(answer, abs(v[0]-v[1]))
+
+    return answer
