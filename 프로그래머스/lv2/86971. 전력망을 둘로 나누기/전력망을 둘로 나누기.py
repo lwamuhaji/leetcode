835715cf5dbe6441d96882441e1381afa6896f2e
lwamuhaji <61176508+lwamuhaji@users.noreply.github.com>
2023-02-04 00:31:08 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 213.74 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.py
+++ 프로그래머스/lv2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.py
@@ -17,10 +17,31 @@ def count_with_bfs(node, wires):
     return count
             
 def solution(n, wires):
+    # 개수 차이의 최솟값. 초기값은 될 수 있는 가장 큰 수 n 으로 지정
     min = n
+    
+    # 모든 간선을 돌면서 하나하나 끊어본다.
     for i in range(len(wires)):
-        count1 = count_with_bfs(wires[i][0], wires[:i] + wires[i+1:])
-        count2 = count_with_bfs(wires[i][1], wires[:i] + wires[i+1:])
-        if min > abs(count1 - count2):
-            min = abs(count1 - count2)
+        # 끊긴 트리를 정의함
+        divided_trees = wires[:i] + wires[i+1:]
+        
+        # 끊겨서 만들어진 두 트리의 노드 개수를 각각 카운트
+        count1 = count_with_bfs(wires[i][0], divided_trees)
+        count2 = count_with_bfs(wires[i][1], divided_trees)
+        
+        # 노드 개수의 차이를 구한다.
+        diff = abs(count1 - count2)
+        
+        # min이 minimum이 아니면 minimum 값으로 바꿈
+        if min > diff:
+            min = diff
     return min
+
+
+def solution(n, wires):
+    ans = n
+    for sub in (wires[i+1:] + wires[:i] for i in range(len(wires))):
+        s = set(sub[0])
+        [s.update(v) for _ in sub for v in sub if set(v) & s]
+        ans = min(ans, abs(2 * len(s) - n))
+    return ans
