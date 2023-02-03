77ab28f95ee0c6a797d1a96ccc710c70577b1f0b
lwamuhaji <61176508+lwamuhaji@users.noreply.github.com>
2023-02-03 17:22:26 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 3.75 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.py
+++ 프로그래머스/lv2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.py
@@ -1,28 +1,38 @@
-uf = []
+from collections import defaultdict, deque
 
-def find(a):
-    global uf
-    if uf[a] < 0: return a
-    uf[a] = find(uf[a])
-    return uf[a]
+def BFS(n, startNode, arr, check):
+    cnt = 1
+    que = deque([startNode])
+    visited = [True for _ in range(n+1)]
+    visited[startNode] = False
 
-def merge(a, b):
-    global uf
-    pa = find(a)
-    pb = find(b)
-    if pa == pb: return
-    uf[pa] += uf[pb]
-    uf[pb] = pa
+    while que:
+        node = que.popleft()
+        for target in arr[node]:
+            if check[node][target] and check[target][node] and visited[target]:
+                visited[target] = False
+                que.append(target)
+                cnt += 1
+
+    return cnt
 
 def solution(n, wires):
-    global uf
-    answer = int(1e9)
-    k = len(wires)
-    for i in range(k):
-        uf = [-1 for _ in range(n+1)]
-        tmp = [wires[x] for x in range(k) if x != i]
-        for a, b in tmp: merge(a, b)
-        v = [x for x in uf[1:] if x < 0]
-        answer = min(answer, abs(v[0]-v[1]))
+    answer = n
+    arr = defaultdict(list)
+    check = [[False for _ in range(n+1)] for _ in range(n+1)]
+
+    for wire in wires:
+        arr[wire[0]].append(wire[1])
+        arr[wire[1]].append(wire[0])
+        check[wire[0]][wire[1]] = True
+        check[wire[1]][wire[0]] = True
+
+    for i, wire in enumerate(wires):
+        a,b = wire
+        check[a][b] = False
+        check[b][a] = False
+        answer = min(answer, abs(BFS(n, a, arr, check) - BFS(n, b, arr, check)))
+        check[a][b] = True
+        check[b][a] = True
 
     return answer
