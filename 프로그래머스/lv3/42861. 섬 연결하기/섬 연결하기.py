e2acbaf616c5d0b78c7d37924ff42827928374fb
lwamuhaji <61176508+lwamuhaji@users.noreply.github.com>
2023-02-08 14:17:36 +0900

    [level 3] Title: 섬 연결하기, Time: 0.11 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv3/42861. 섬 연결하기/섬 연결하기.py
+++ 프로그래머스/lv3/42861. 섬 연결하기/섬 연결하기.py
@@ -1,28 +1,3 @@
-from collections import deque
-
-
-# 트리를 node1부터 DFS로 순회하며 node1과 node2가 연결되어 있는지 탐색
-def is_connected_with(node1, node2, edges):
-    visited = [False for _ in range(100)]
-    queue = deque([node1])
-    while queue:
-        current_node = queue.popleft()
-        visited[current_node] = True
-        if current_node == node2:
-            return True
-        for edge in edges:
-            if edge[0] == current_node and not visited[edge[1]]:
-                queue.append(edge[1])
-            if edge[1] == current_node and not visited[edge[0]]:
-                queue.append(edge[0])
-    return False
-
-def solution(n, costs: list):
-    costs.sort(key=lambda x: x[2])
-    used = []
-
-    for cost in costs:
-        if not is_connected_with(cost[0], cost[1], used):
-            used.append(cost)
-    
-    return sum(cost[2] for cost in used)
+def solution(n, costs):
+    answer = 0
+    return answer
