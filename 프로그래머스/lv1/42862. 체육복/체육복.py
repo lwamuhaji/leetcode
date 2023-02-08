c6b2f05d63fded0244f2a7b0f41221ed755d22e1
lwamuhaji <61176508+lwamuhaji@users.noreply.github.com>
2023-02-08 23:05:30 +0900

    [level 1] Title: 체육복, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv1/42862. 체육복/체육복.py
+++ 프로그래머스/lv1/42862. 체육복/체육복.py
@@ -1,4 +1,8 @@
 def solution(n, lost, reserve):
+    '''
+    [마지막 제한사항 대응]
+    집합으로 만들어서 lost와 reserve의 교집합을 구하고 각각 뺀다.
+    '''
     for k in set(lost) & set(reserve):
         lost.remove(k)
         reserve.remove(k)
