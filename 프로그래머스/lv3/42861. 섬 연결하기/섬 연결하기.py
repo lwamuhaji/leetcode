2e7d3306c2580529bc00ef5cb3563ed7b2ce1500
lwamuhaji <61176508+lwamuhaji@users.noreply.github.com>
2023-02-12 19:13:40 +0900

    [level 3] Title: 섬 연결하기, Time: 0.01 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/lv3/42861. 섬 연결하기/섬 연결하기.py
+++ 프로그래머스/lv3/42861. 섬 연결하기/섬 연결하기.py
@@ -1,18 +1,25 @@
-def ancestor(node, parents):
+# parents에서 node의 부모 찾기
+def get_parent(parents, node):
+    # 부모가 자기 자신이면 자기 자신 반환
     if parents[node] == node:
         return node
-    else:
-        return ancestor(parents[node], parents)
+    # 부모가 다른 노드이면 그 노드의 부모를 반환
+    return get_parent(parents, parents[node])
 
 def solution(n, costs):
     answer = 0
-    edges = sorted([(x[2], x[0], x[1]) for x in costs])
-    parents = [i for i in range(n)]
+    costs.sort(key=lambda x: x[2])    
+    parents = [_ for _ in range(n)]
+    
     bridges = 0
-    for w, f, t in edges:
-        if ancestor(f, parents) != ancestor(t, parents):
-            answer += w
-            parents[ancestor(f, parents)] = t
+    for cost in costs:
+        # 연결하려는 두 부모의 노드가 다른 경우
+        if get_parent(parents, cost[0]) != get_parent(parents, cost[1]):
+            # 연결한다.
+            answer += cost[2]
+            
+            # 
+            parents[get_parent(parents, cost[0])] = cost[1]
             bridges += 1
         if bridges == n - 1:
             break
