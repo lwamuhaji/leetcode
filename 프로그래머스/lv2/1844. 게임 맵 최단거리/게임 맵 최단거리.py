108e2e8fcaa2850ec2c1791d8428ac1407a283f4
lwamuhaji <61176508+lwamuhaji@users.noreply.github.com>
2023-02-28 23:09:25 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 5.89 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/lv2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
+++ 프로그래머스/lv2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
@@ -3,44 +3,21 @@ from collections import deque
 adjs = [(1, 0), (-1, 0), (0, 1), (0, -1)]
 
 def solution(maps):
-    m = len(maps)
-    n = len(maps[0])
-    visited = [[0 for x in range(n)] for y in range(m)]
-    depths = [[0 for x in range(n)] for y in range(m)]
+    visited = [[0 for x in range(len(maps[0]))] for y in range(len(maps))]
+    depths = [[0 for x in range(len(maps[0]))] for y in range(len(maps))]
     queue = deque([(0,0)])
     
     while queue:
         current = queue.popleft()
         visited[current[0]][current[1]] = 1
+        
         for x, y in adjs:
             next_x = current[0] + x
             next_y = current[1] + y
-            if next_x >= 0 and next_y >= 0 and next_x < m and next_y < n and maps[next_x][next_y] and not visited[next_x][next_y]:
+            if next_x >= 0 and next_y >= 0 and next_x < len(maps) and next_y < len(maps[0]) and maps[next_x][next_y] and not visited[next_x][next_y]:
                 depths[next_x][next_y] = depths[current[0]][current[1]] + 1
                 queue.append((next_x, next_y))
-    answer = depths[m-1][n-1]
-    if answer == 0:
-        return -1
-    return answer + 1
-
-def solution(maps):
-    m = len(maps)
-    n = len(maps[0])
-    visited = [[0 for x in range(n)] for y in range(m)]
-    depths = [[0 for x in range(n)] for y in range(m)]
-    queue = deque([(0,0)])
-    
-    while queue:
-        current = queue.popleft()
-        visited[current[0]][current[1]] = 1
-        for x, y in adjs:
-            next_x = current[0] + x
-            next_y = current[1] + y
-            if next_x >= 0 and next_y >= 0 and next_x < m and next_y < n and maps[next_x][next_y] and not visited[next_x][next_y]:
-                visited[next_x][next_y] = 1
-                depths[next_x][next_y] = depths[current[0]][current[1]] + 1
-                queue.append((next_x, next_y))
-    answer = depths[m-1][n-1]
+    answer = depths[len(maps)-1][len(maps[0])-1]
     if answer == 0:
         return -1
     return answer + 1
