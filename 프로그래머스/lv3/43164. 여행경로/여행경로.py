3aec1ecac5a006ebca8cf5801ba4eddcddc153ed
lwamuhaji <61176508+lwamuhaji@users.noreply.github.com>
2023-03-05 23:33:25 +0900

    [level 3] Title: 여행경로, Time: 0.02 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv3/43164. 여행경로/여행경로.py
@@ -0,0 +1,30 @@
+
+
+def solution(tickets):
+    def adjacencies(node):
+        return [i for i in range(len(tickets)) if tickets[node][1] == tickets[i][0]]
+        
+    def dfs(node, visited, routes):
+        if len(routes) == len(tickets):
+            result.append([tickets[routes[0]][0]] + [tickets[ticket][1] for ticket in routes])
+            
+        for next_node in sorted(adjacencies(node), key=lambda x: tickets[x]):
+            if not visited[next_node]:
+                visited_temp = visited[:]
+                visited_temp[next_node] = True
+                
+                routes_temp = routes[:]
+                routes_temp.append(next_node)
+                
+                dfs(next_node, visited_temp, routes_temp)
+    
+    result = []
+    for i in range(len(tickets)):
+        if tickets[i][0] == "ICN":
+            visited = [False for n in range(len(tickets))]
+            visited[i] = True
+            routes = [i]
+            dfs(i, visited, routes)
+    return sorted(result)[0]
+    
+    
