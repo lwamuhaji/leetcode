1518694f7805af3e8fafd144606776f56ec4b28b
lwamuhaji <61176508+lwamuhaji@users.noreply.github.com>
2023-03-07 18:20:15 +0900

    [level 0] Title: 다음에 올 숫자, Time: 0.01 ms, Memory: 10 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120924. 다음에 올 숫자/다음에 올 숫자.py
@@ -0,0 +1,5 @@
+def solution(common):
+    if common[0] + common[2] == common[1] * 2:
+        return common[-1] + common[1] - common[0]
+    if common[0] * common[2] == common[1]**2:
+        return common[-1] * (common[1] / common[0])
