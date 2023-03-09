f1865a578a97251d327e4fa8bf71f41a4d1b70a7
lwamuhaji <61176508+lwamuhaji@users.noreply.github.com>
2023-03-09 23:15:58 +0900

    [level 0] Title: 평행, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv0/120875. 평행/평행.py
+++ 프로그래머스/lv0/120875. 평행/평행.py
@@ -1,6 +1,4 @@
 def solution(dots):
-    # 1 2
     if abs((dots[0][0] - dots[1][0]) / (dots[0][1] - dots[1][1])) == abs((dots[2][0] - dots[3][0]) / (dots[2][1] - dots[3][1])):
         return 1
-    abs(dots[0][0] - dots[2][0] / dots[0][1] - dots[2][1]) == abs(dots[1][0] - dots[3][0] / dots[1][1] - dots[3][1])
     return 0
