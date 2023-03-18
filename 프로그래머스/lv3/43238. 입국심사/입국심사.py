ea252940f086f9b7ee34a19af46bce04697da093
lwamuhaji <61176508+lwamuhaji@users.noreply.github.com>
2023-03-18 21:40:12 +0900

    [level 3] Title: 입국심사, Time: 0.04 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv3/43238. 입국심사/입국심사.py
+++ 프로그래머스/lv3/43238. 입국심사/입국심사.py
@@ -1,12 +1,10 @@
 def solution(n, times):
-    low, high = min(times), max(times) * n
+    low, high = times[0], times[-1] * n
     mid = (low + high) // 2
-    
     while (mid != high):
         if sum([mid//time for time in times]) < n:
             low = mid + 1
         else:
             high = mid
         mid = (low + high) // 2
-        
     return mid
