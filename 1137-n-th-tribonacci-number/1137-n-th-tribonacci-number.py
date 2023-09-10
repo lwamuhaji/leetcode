4e3ad3c9306e9a634d490573d5e80aadf4531b54
lwamuhaji <61176508+lwamuhaji@users.noreply.github.com>
2023-09-10 23:14:38 +0900

    Time: 33 ms (87.07%), Space: 16.3 MB (27.48%) - LeetHub

--- 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.py
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.py
@@ -6,6 +6,21 @@ class Solution:
         if n < 3:
             return 1
         
+        sheet = [_ for _ in range(n+1)]
+        sheet[0] = 0
+        sheet[1] = 1
+        sheet[2] = 1
+        
+        for i in range(3, n+1):
+            sheet[i] = sheet[i-3]+sheet[i-2]+sheet[i-1]
+        return sheet[n]
+    
+    def tribonacci2(self, n: int) -> int:
+        if n == 0:
+            return 0
+        if n < 3:
+            return 1
+        
         if k := self.memo.get(n):
             return k
         
