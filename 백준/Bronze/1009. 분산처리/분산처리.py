c7ce5189f6c43616654f0d1b7fb2b67207a09fee
lwamuhaji <61176508+lwamuhaji@users.noreply.github.com>
2025-03-05 23:09:39 +0900

    [Bronze II] Title: 분산처리, Time: 52 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/1009. 분산처리/분산처리.py
+++ 백준/Bronze/1009. 분산처리/분산처리.py
@@ -2,5 +2,4 @@ N = int(input())
 l = [[10], [1], [2, 4, 8, 6], [3, 9, 7, 1], [4, 6], [5], [6], [7, 9, 3, 1], [8, 4, 2, 6], [9, 1]]
 for _ in range(N):
     a, b = map(int, input().split())
-    n = a%10
-    print(l[n][(b-1)%len(l[n])])
+    print(l[a%10][(b-1)%len(l[a%10])])
