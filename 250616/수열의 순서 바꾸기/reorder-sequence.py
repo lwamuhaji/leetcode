aa12b270b0ca391da9f0fb789a19e331217746ea
lwamuhaji <61176508+lwamuhaji@users.noreply.github.com>
2025-06-16 13:43:24 +0900

    Title: 수열의 순서 바꾸기, Time: 45ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 250616/수열의 순서 바꾸기/reorder-sequence.py
@@ -0,0 +1,11 @@
+n = int(input())
+sequence = list(map(int, input().split()))
+
+# Please write your code here.
+pos = 0
+for i in range(len(sequence)-1):
+    if sequence[i] < sequence[i+1]:
+        ...
+    else:
+        pos = i + 1
+print(pos)
